77931608e492fc98746aed01e53a1b56f25fe9fe
nasry <nasri.ashref@gmail.com>
2016-11-08 11:37:31 +0100

    third commit

--- conf/startall.sh
+++ conf/startall.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 cd /home/test/IMRT/conf/
 echo Starting all applications on: '192.168.1.60'
-if [ -e ./start_HarvestControllerApplication_snapshot1.sh ]; then 
-      ./start_HarvestControllerApplication_snapshot1.sh
-fi
 if [ -e ./start_HarvestControllerApplication_focused1.sh ]; then 
       ./start_HarvestControllerApplication_focused1.sh
 fi
-if [ -e ./start_HarvestControllerApplication_focused2.sh ]; then 
-      ./start_HarvestControllerApplication_focused2.sh
-fi
-if [ -e ./start_HarvestControllerApplication_focused3.sh ]; then 
-      ./start_HarvestControllerApplication_focused3.sh
-fi
-if [ -e ./start_HarvestControllerApplication_focused4.sh ]; then 
-      ./start_HarvestControllerApplication_focused4.sh
-fi
+
+
+
+
+
+
