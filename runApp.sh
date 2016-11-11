c45e90f547f6522827a5d1ce76eaf1469aed27db
nasry <nasri.ashref@gmail.com>
2016-11-11 16:29:25 +0100

    Update runApp.sh

--- runApp.sh
+++ runApp.sh
@@ -14,8 +14,9 @@ sed -i "s/8046/$p1/g" /home/test/IMRT/conf/settings_HarvestControllerApplication
 sed -i "s/8192/$p2/g" /home/test/IMRT/conf/settings_HarvestControllerApplication_focused1.xml;
 
 # change port in the service
-ssh root@192.168.1.205 "kubectl get service/$app -o json  | jq  '.spec.ports[0].port = $p1 | .spec.ports[0].targetPort = $p1 | .spec.ports[1].port = $p2 | .spec.ports[1].targetPort = $p2' > service.json ; kubectl replace  -f service.json"
-
+#ssh root@192.168.1.205 "kubectl get service/$app -o json  | jq  '.spec.ports[0].port = $p1 | .spec.ports[0].targetPort = $p1 | .spec.ports[1].port = $p2 | .spec.ports[1].targetPort = $p2' > service.json ; kubectl replace  -f service.json"
+http://192.168.1.205:8080/api/v1/namespaces/default/services/$app | jq  '.spec.ports[0].port = $p1 | .spec.ports[0].targetPort = $p1 | .spec.ports[1].port = $p2 | .spec.ports[1].targetPort = $p2' > harvester-auto.json;
+curl http://192.168.1.205:8080/api/v1/namespaces/default/services/$app -XPUT -H'Content-Type: application/json' -d@harvester-auto.json
 
 #echo "Sourcing /etc/profile ...";
 #source /etc/profile;
@@ -31,10 +32,11 @@ echo "Add admin, access-server and bitarkive to /etc/hosts ...";
 echo -e "192.168.1.51    admin \n192.168.1.52    bitarchieve \n192.168.1.59    access-server" >> /etc/hosts;
 
 
-echo "Add the container hostname < $HOSTNAME > and its IP in the admin, access-server and bitarvieve to known as harvester ...";
-ssh test@192.168.1.51 "echo 192.168.1.205    $HOSTNAME >> /etc/hosts";
-ssh test@192.168.1.52 "echo 192.168.1.205    $HOSTNAME >> /etc/hosts";
-ssh test@192.168.1.59 "echo 192.168.1.205    $HOSTNAME >> /etc/hosts";
+#echo "Add the container hostname < $HOSTNAME > and its IP in the admin, access-server and bitarvieve to known as harvester ...";
+#ssh test@192.168.1.51 "echo 192.168.1.205    $HOSTNAME >> /etc/hosts";
+#ssh test@192.168.1.52 "echo 192.168.1.205    $HOSTNAME >> /etc/hosts";
+#ssh test@192.168.1.59 "echo 192.168.1.205    $HOSTNAME >> /etc/hosts";
 
-echo "Start harvester ...";
+echo "Start harvester & MQ ...";
 /home/test/IMRT/conf/startall.sh;
+/MessageQueue5.1/mq/bin/imqbrokerd -reset store -tty;
